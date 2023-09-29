445b152cb4034cc1bca756f71d783b77993dccfc
gentlemanSoup <anders.horne@gmail.com>
2023-09-29 18:56:17 +0200

    Update ChangelogMenu.cs

--- Runtime/ChangelogMenu.cs
+++ Runtime/ChangelogMenu.cs
@@ -1,5 +1,8 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace Pixygon.Versioning {
     public class ChangelogMenu : MonoBehaviour {
@@ -7,21 +10,24 @@ namespace Pixygon.Versioning {
         [SerializeField] private GameObject _changelogPanel;
         [SerializeField] private TextMeshProUGUI _changelogText;
         [SerializeField] private RectTransform _textParent;
+        [SerializeField] private UnityEvent _onCloseAction;
+        [SerializeField] private GameObject _firstActive;
         private int _currentChangeLog;
         private VersionData[] _versions;
+
+        public UnityEvent OnCloseAction => _onCloseAction;
 
         public void Initialize(VersionData[] versions) {
             _versions = versions;
             _versionText.text = "v" + _versions[_versions.Length - 1].Version;
         }
-        public void OpenVersionLog()
-        {
+        public void OpenVersionLog() {
             _currentChangeLog = _versions.Length - 1;
             _changelogPanel.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(_firstActive);
             SetVersionLog();
         }
-        public void MoveVersionLog(int i)
-        {
+        public void MoveVersionLog(int i) {
             if (i == -1) {
                 if (_currentChangeLog == 0) _currentChangeLog = _versions.Length - 1;
                 else _currentChangeLog -= 1;
@@ -31,10 +37,14 @@ namespace Pixygon.Versioning {
             }
             SetVersionLog();
         }
-
         private void SetVersionLog() {
             _changelogText.text = _versions[_currentChangeLog].Changelog;
             _textParent.sizeDelta = new Vector2(_textParent.sizeDelta.x, _changelogText.preferredHeight+100f);
+        }
+
+        public void OnCloseVersionLog() {
+            _changelogPanel.SetActive(false);
+            OnCloseAction?.Invoke();
         }
     }
 }
